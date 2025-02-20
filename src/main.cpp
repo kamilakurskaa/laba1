8f90ceb3107b0b7d10ea6fa179fe2c4f73985230
Камила <@WIN-4LLG537Q8J3>
2025-02-20 15:33:50 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,10 @@ int subtract(int a, int b) {
 
 int main() {
 	int a, b;
-	std::cout << "Write 1st number: ";
+	std::cout << "1st number: ";
 	std::cin >> a;
-	std::cout << "Write 2nd number: ";
+	std::cout << "2nd number: ";
 	std::cin >> b;
-	std::cout << "Addition: " << add(a, b) << std::endl;
-	std::cout << "Subtraction: " << subtract(a, b) << std::endl;
+	std::cout << "Addition:" << add(a, b) << std::endl;
+	std::cout << "Subtraction:" << subtract(a, b) << std::endl;
 }
